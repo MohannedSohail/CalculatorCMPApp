fd77addc2b66ad5ca7afb61508f110a82e1c917b
Mohanned Sohail <87503924+MohannedSohail@users.noreply.github.com>
2025-05-04 16:27:05 +0300

    android-ci.yml Fix Bugs

--- composeApp/src/commonMain/kotlin/org/mohanned/calculatorcmpapp/App.kt
+++ composeApp/src/commonMain/kotlin/org/mohanned/calculatorcmpapp/App.kt
@@ -4,12 +4,14 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -22,7 +24,7 @@ fun App() {
     MaterialTheme {
         var showContent by remember { mutableStateOf(false) }
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
+            Button(modifier = Modifier.fillMaxWidth().padding(horizontal = 10.dp, vertical = 30.dp), onClick = { showContent = !showContent }) {
                 Text("Click me!")
             }
             AnimatedVisibility(showContent) {
